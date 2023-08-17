abe69abb5aaf4072b5c50c7318d292c7aaa901d8
aymen-boukil <aymenbk08@gmail.com>
2023-08-17 11:32:57 -0700

    aymen

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,52 +2,39 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
-/**
- * print_all - Prints any data type followed by a new line.
- * @format: A list of types of arguments passed to the function.
- */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
 	unsigned int i = 0;
-
-	char *separator = "";
+	char *sep = "";
 
 	va_start(args, format);
-
 	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
+				printf("%s%c", sep, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
+				printf("%s%d", sep, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
+				printf("%s%f", sep, va_arg(args, double));
 				break;
 			case 's':
+				printf("%s", sep);
 				{
 					char *str = va_arg(args, char *);
-
-					if (str == NULL)
-						str = "(nil)";
-					printf("%s%s", separator, str);
-					break;
+					str = (str == NULL) ? "(nil)" : str;
+					printf("%s", str);
 				}
-			default:
-				i++;
-				continue;
+				break;
 		}
-		separator = ", ";
+		sep = ", ";
 		i++;
 	}
-
 	printf("\n");
-
 	va_end(args);
 }
 
