70586ca46feef94b08b81bec2ed1d4c41561644c
aymen-boukil <aymenbk08@gmail.com>
2023-10-04 07:17:34 -0700

    aymen

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	int i;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	/* Calculate the length of the string */
 	while (str[length])
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 	duplicate = (char *)malloc((length + 1) * sizeof(char));
 
 	if (duplicate == NULL)
-		return NULL;
+		return (NULL);
 
 	/* Copy the original string to the duplicate */
 	for (i = 0; i < length; i++)
@@ -33,6 +33,6 @@ char *_strdup(char *str)
 	/* Null-terminate the duplicate string */
 	duplicate[length] = '\0';
 
-	return duplicate;
+	return (duplicate);
 }
 
