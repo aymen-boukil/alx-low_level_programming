8176dddebe39465dfb09d27abdd3aacc1ad7634e
aymen-boukil <aymenbk08@gmail.com>
2023-10-05 06:13:20 -0700

    aymen

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: The argument count
- * @argv: The argument vector, an array of strings
+ * main - Multiplies two numbers.
+ * @argc: Argument count
+ * @argv: Arguments
  *
- * Return: Always 0
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-    int x, y;
+	int x, y;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    x = atoi(argv[1]);
-    y = atoi(argv[2]);
-    printf("%d\n", x * y);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
-    return 0;
+	return (0);
 }
 
