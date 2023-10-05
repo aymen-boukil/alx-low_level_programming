a7705c3016d19432d7887a32767daaa7184fc191
aymen-boukil <aymenbk08@gmail.com>
2023-10-05 05:46:16 -0700

    aymen

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector, an array of strings
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
+    if (argc > 0)
+        printf("%s\n", argv[0]);
+
     return 0;
 }
 
