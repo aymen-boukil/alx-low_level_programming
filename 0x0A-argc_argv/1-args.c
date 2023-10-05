67acf5729be5a44b020aa84d32f257e81445726d
aymen-boukil <aymenbk08@gmail.com>
2023-10-05 05:58:20 -0700

    aymen

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector, an array of strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv; /* Unused parameter */
+
+	printf("%d\n", argc - 1);
+	return 0;
+}
+
